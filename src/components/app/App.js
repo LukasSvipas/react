2f5af399ea499bfb8aad8fd0a675f249c71e5966
LukasSvipas <113436969+LukasSvipas@users.noreply.github.com>
2022-11-14 15:12:30 +0200

    Add files via upload

---
+++ src/components/app/App.js
@@ -0,0 +1,16 @@
+import Header from "../header/Header";
+import Nav from "../nav/Nav";
+import Main from "../main/Main";
+import Footer from "../footer/Footer";
+function App() {
+  return (
+    <>
+        <Nav/>
+        <Header/>
+        <Main/>
+        <Footer/>
+        </>
+  );
+}
+
+export default App;
